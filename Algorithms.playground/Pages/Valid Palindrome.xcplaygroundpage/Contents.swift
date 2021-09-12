f21a416cff3a163e058965ca9e5b5f480a956770
Chuy <chuynadamas@gmail.com>
2021-09-12 16:47:08 -0500

    Valid Palindrome

--- Algorithms.playground/Pages/Valid Palindrome.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Valid Palindrome.xcplaygroundpage/Contents.swift
@@ -2,6 +2,33 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func isPalindrome(_ s: String) -> Bool {
+    //Clean the string
+    let allowedChart : Set<Character> = Set("abcdefghijklmnopqrstuvwxyz0123456789")
+    let s_clean = s.lowercased().filter({ allowedChart.contains($0) })
+    let p = String(s_clean.reversed())
+        
+    //Then we can compare directly
+    if p == s_clean {
+        print("Is a Palindrome")
+    }
+    
+    print(s_clean)
+    print(p)
+    
+    //Or manually
+    for i in 0..<s_clean.count {
+        let index = s.index(s.startIndex, offsetBy: i)
+        if p[index] != s_clean[index] {
+            //There is a different character, not a palindrome
+            return false
+        }
+    }
+    // The comparison was done is a palindrome
+    return true
+}
 
+
+
+isPalindrome("A man, a plan, a canal: Panama")
 //: [Next](@next)
