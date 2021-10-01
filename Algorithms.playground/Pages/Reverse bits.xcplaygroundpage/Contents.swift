2dd55a107f34a65eac33bcc7346e6ac31075a4b7
Chuy <chuynadamas@gmail.com>
2021-09-30 20:23:20 -0500

    Reverse Bits

---
+++ Algorithms.playground/Pages/Reverse bits.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+func reverseBits(_ n: Int) -> Int {
+    var bits = 0
+    var n = n
+    
+    for i in 0..<32 {
+        bits = bits | ( n & 1)
+        
+        if i != 31 {
+            n = n >> 1
+            bits = bits << 1
+        }
+    }
+    return bits
+}
+
+let test = 0b00000010100101000001111010011100
+reverseBits(test)
+
+//: [Next](@next)
