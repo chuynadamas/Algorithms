2515508ed55e2ecc619acbf9dc1445bcdab1a440
Chuy <chuynadamas@gmail.com>
2021-09-30 22:53:23 -0500

    Enhance algorithm

--- Algorithms.playground/Pages/Reverse bits.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Reverse bits.xcplaygroundpage/Contents.swift
@@ -7,13 +7,10 @@ func reverseBits(_ n: Int) -> Int {
     var bits = 0
     var n = n
     
-    for i in 0..<32 {
+    for _ in 0..<32 {
+        bits = bits << 1
         bits = bits | ( n & 1)
-        
-        if i != 31 {
-            n = n >> 1
-            bits = bits << 1
-        }
+        n = n >> 1
     }
     return bits
 }
