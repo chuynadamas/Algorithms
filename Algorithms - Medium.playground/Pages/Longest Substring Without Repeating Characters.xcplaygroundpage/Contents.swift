da5eb53c35368617efa0d3b038b07e6710523237
Chuy <chuynadamas@gmail.com>
2021-10-09 17:03:44 -0500

    Longes Substring

---
+++ Algorithms - Medium.playground/Pages/Longest Substring Without Repeating Characters.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+import CoreGraphics
+import Darwin
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    
+    var charArray = Array(s)
+    var dict = [Character: Int]()
+    var l = 0
+    
+    var ans = 0
+    
+    //Keep opening the window
+    for r in 0..<charArray.count {
+        print(dict)
+        while dict[charArray[r]] != nil && dict[charArray[r]]! != 0 {
+            //Remove the duplicated character
+            dict[charArray[l]]! -= 1
+            //Close the window
+            l += 1
+        }
+        //Add unique character
+        dict[charArray[r]] = 1
+        //Validate the window size
+        ans = max(ans, r - l + 1)
+    }
+    return ans
+}
+
+
+lengthOfLongestSubstring("pwwkew")
+
+//: [Next](@next)
