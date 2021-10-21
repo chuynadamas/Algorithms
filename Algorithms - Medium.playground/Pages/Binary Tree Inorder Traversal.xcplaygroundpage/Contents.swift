436c6b01115b2db9c550879a588d87bee7f2d1e6
Chuy <chuynadamas@gmail.com>
2021-10-20 21:25:22 -0500

    Binary Tree Inorder Traversal

---
+++ Algorithms - Medium.playground/Pages/Binary Tree Inorder Traversal.xcplaygroundpage/Contents.swift
@@ -0,0 +1,34 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    var result = [Int]()
+    inOrder(node: root, result: &result)
+    return result
+}
+
+func inOrder(node: TreeNode?, result: inout [Int]) {
+    if node == nil { return }
+    
+    inOrder(node: node?.left, result: &result)
+    result.append(node!.val)
+    inOrder(node: node?.right, result: &result)
+}
+
+
+//: [Next](@next)
