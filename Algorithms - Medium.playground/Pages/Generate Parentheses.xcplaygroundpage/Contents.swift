10aaefab8d66e91a228df2190403ecd63f1ba87d
Chuy <chuynadamas@gmail.com>
2021-11-14 22:56:56 -0600

    Generating Parenthesis

---
+++ Algorithms - Medium.playground/Pages/Generate Parentheses.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+import Darwin
+
+func generateParenthesis(_ n: Int) -> [String] {
+    var stack : [String] = []
+    var result : [String] = []
+    
+    func backtrack(_ open: Int, _ close:Int) {
+        //handling base scenario
+        if open == close && close == n {
+            print(stack)
+            result.append(stack.joined(separator: ""))
+            return
+        }
+        
+        if open < n {
+            stack.append("(")
+            backtrack(open+1, close)
+            stack.popLast()
+        }
+        
+        if close < open {
+            stack.append(")")
+            backtrack(open, close + 1)
+            stack.popLast()
+        }
+    }
+    
+    backtrack(0,0)
+    return result
+}
+
+generateParenthesis(3)
+
+//: [Next](@next)
