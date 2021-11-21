347d9aa0a13dc1ee7f54c31eeb351fda70427c1c
Chuy <chuynadamas@gmail.com>
2021-11-20 23:13:23 -0600

    Top K Frequent Elements

--- Algorithms - Medium.playground/Pages/Top Kth Elements.xcplaygroundpage/Contents.swift
+++ Algorithms - Medium.playground/Pages/Top Kth Elements.xcplaygroundpage/Contents.swift
@@ -2,6 +2,34 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+    
+    var ocurrences : [Int:Int] = [:]
+    var freq: [[Int]] = Array(repeating: [], count: nums.count+1)
+    
+    for num in nums {
+        if let tableValue = ocurrences[num] {
+            ocurrences[num] = tableValue + 1
+        } else {
+            ocurrences[num] = 1
+        }
+    }
+
+    for (key, value) in ocurrences {
+        freq[value].append(key)
+    }
+    
+    var result = [Int]()
+    
+    for i in (0..<freq.count).reversed() {
+        result.append(contentsOf: freq[i])
+        if result.count == k {
+            return result
+        }
+    }
+    return result
+}
+
+topKFrequent([-1,-1],1)
 
 //: [Next](@next)
