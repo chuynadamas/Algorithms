f238258239db4949198b46731b8ee758e2910115
Chuy <chuynadamas@gmail.com>
2021-11-27 09:58:21 -0600

    Improve algorithms

--- Algorithms - Hard.playground/Pages/Regular Expression Matching.xcplaygroundpage/Contents.swift
+++ Algorithms - Hard.playground/Pages/Regular Expression Matching.xcplaygroundpage/Contents.swift
@@ -7,7 +7,14 @@ func isMatch(_ s: String, _ p: String) -> Bool {
     let s = Array(s)
     let p = Array(p)
     
+    var cache : [String: Bool] = [:]
+    
     func backtracking(_ i: Int,_ j: Int) -> Bool {
+        
+        if let value = cache[String(i) + String(j)] {
+            return value
+        }
+        
         if i >= s.count && j >= p.count { return true }
         
         if j >= p.count { return false }
@@ -15,21 +22,21 @@ func isMatch(_ s: String, _ p: String) -> Bool {
         let match = i < s.count && (s[i] == p[j] || p[j] == ".")
         
         if (j+1) < p.count && p[j+1] == "*" {
-            return backtracking(i, j + 2) || backtracking(i + 1, j)
+            cache[String(i) + String(j)] = backtracking(i, j + 2) || (match && backtracking(i + 1, j))
+            return cache[String(i) + String(j)]!
         }
         
         if match {
-            return backtracking(i+1, j+1)
+            cache[String(i) + String(j)] = backtracking(i+1, j+1)
+            return cache[String(i) + String(j)]!
         }
-        
+        cache[String(i) + String(j)] = false
         return false
     }
     
     return backtracking(0,0)
 }
 
-
 isMatch("si","s*")
-
 //: [Next](@next)
 
