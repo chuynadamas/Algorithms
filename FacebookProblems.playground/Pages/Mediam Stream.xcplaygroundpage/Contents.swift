ef0419f27285457edc062014a763e5d2e9d6ff15
Chuy <chuynadamas@gmail.com>
2021-12-08 21:34:14 -0600

    Media stream doc

--- FacebookProblems.playground/Pages/Mediam Stream.xcplaygroundpage/Contents.swift
+++ FacebookProblems.playground/Pages/Mediam Stream.xcplaygroundpage/Contents.swift
@@ -1,5 +1,36 @@
 //: [Previous](@previous)
+/**
+ 
+ Median Stream
+ You're given a list of n integers arr[0..(n-1)]. You must compute a list output[0..(n-1)] such that, for each index i (between 0 and n-1, inclusive), output[i] is equal to the median of the elements arr[0..i] (rounded down to the nearest integer).
+ 
+ The median of a list of integers is defined as follows. If the integers were to be sorted, then:
+ If there are an odd number of integers, then the median is equal to the middle integer in the sorted order.
+ Otherwise, if there are an even number of integers, then the median is equal to the average of the two middle-most integers in the sorted order.
+ Signature
+ int[] findMedian(int[] arr)
+ 
+ Input
+ n is in the range [1, 1,000,000].
+ Each value arr[i] is in the range [1, 1,000,000].
+ 
+ Output
+ Return a list of n integers output[0..(n-1)], as described above.
+ 
+ Example 1
+ n = 4
+ arr = [5, 15, 1, 3]
+ output = [5, 10, 5, 4]
+ The median of [5] is 5, the median of [5, 15] is (5 + 15) / 2 = 10, the median of [5, 15, 1] is 5, and the median of [5, 15, 1, 3] is (3 + 5) / 2 = 4.
+ 
+ Example 2
+ n = 2
+ arr = [1, 2]
+ output = [1, 1]
+ The median of [1] is 1, the median of [1, 2] is (1 + 2) / 2 = 1.5 (which should be rounded down to 1).
+ 
 
+ */
 import Foundation
 
 struct Heap<Element> {
---
+++ FacebookProblems.playground/Pages/Slow Sums.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+/*
+ Slow Sums
+ Suppose we have a list of N numbers, and repeat the following operation until we're left with only a single number: Choose any two numbers and replace them with their sum. Moreover, we associate a penalty with each operation equal to the value of the new number, and call the penalty for the entire list as the sum of the penalties of each operation.
+ For example, given the list [1, 2, 3, 4, 5], we could choose 2 and 3 for the first operation, which would transform the list into [1, 5, 4, 5] and incur a penalty of 5. The goal in this problem is to find the highest possible penalty for a given input.
+ Signature:
+ int getTotalTime(int[] arr)
+ 
+ Input:
+ An array arr containing N integers, denoting the numbers in the list.
+ 
+ Output format:
+ An int representing the highest possible total penalty.
+ 
+ Constraints:
+ 1 ≤ N ≤ 10^6
+ 1 ≤ Ai ≤ 10^7, where *Ai denotes the ith initial element of an array.
+ The sum of values of N over all test cases will not exceed 5 * 10^6.
+ 
+ Example
+ arr = [4, 2, 1, 3]
+ output = 26
+ First, add 4 + 3 for a penalty of 7. Now the array is [7, 2, 1]
+ Add 7 + 2 for a penalty of 9. Now the array is [9, 1]
+ Add 9 + 1 for a penalty of 10. The penalties sum to 26.
+ */
+import Foundation
+
+func getTotalTime(arr: [Int]) -> Int {
+    // Write your code here
+    let sorted = arr.sorted(by: >)
+    
+    var results: [Int] = [sorted[0]+sorted[1]]
+    for index in 2..<sorted.count {
+        results.append(results.last! + sorted[index])
+    }
+    
+    print(results)
+    return results.reduce(.zero, +);
+}
+
+getTotalTime(arr: [4, 2, 1, 3])
+//: [Next](@next)
