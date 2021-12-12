b4c4ef1bacc85a394189581b9859f1c455eccea7
Chuy <chuynadamas@gmail.com>
2021-12-12 13:15:41 -0600

    FB medium problem

---
+++ Algorithms - Medium.playground/Pages/Product of Array Except Self.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+/**
+ Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ 
+ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+ You must write an algorithm that runs in O(n) time and without using the division operation.
+ 
+ Example 1:
+ 
+ Input: nums = [1,2,3,4]
+ Output: [24,12,8,6]
+ Example 2:
+ 
+ Input: nums = [-1,1,0,-3,3]
+ Output: [0,0,9,0,0]
+ 
+ Constraints:
+ 
+ 2 <= nums.length <= 105
+ -30 <= nums[i] <= 30
+ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ */
+import Foundation
+
+func productExceptSelf(_ nums: [Int]) -> [Int] {
+    var result = Array(repeating: 1, count: nums.count)
+    var prefix = 1
+    
+    for i in 0..<result.count {
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+    var postfix = 1
+    for i in (0..<result.count).reversed() {
+        result[i] *= postfix
+        postfix *= nums[i]
+    }
+    return result
+}
+
+
+productExceptSelf([1,2,3,4])
+//: [Next](@next)
