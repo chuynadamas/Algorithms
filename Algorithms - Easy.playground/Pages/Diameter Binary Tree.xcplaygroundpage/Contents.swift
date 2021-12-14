276d58b35d4579629ad5c0ca4b202dcd0a091ede
Chuy <chuynadamas@gmail.com>
2021-12-13 22:10:48 -0600

    Tree problems

---
+++ Algorithms - Easy.playground/Pages/Diameter Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+//: [Previous](@previous)
+/**
+ 
+ Given the root of a binary tree, return the length of the diameter of the tree.
+
+ The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+ The length of a path between two nodes is represented by the number of edges between them.
+  
+
+ Example 1:
+
+
+ Input: root = [1,2,3,4,5]
+ Output: 3
+ Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+ Example 2:
+
+ Input: root = [1,2]
+ Output: 1
+ 
+ */
+import Foundation
+
+func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+    if root == nil {
+        return 0
+    }
+    return getLongesthPath(root!.left) + getLongesthPath(root!.right)
+}
+
+func getLongesthPath(_ node: TreeNode?) -> Int {
+    if node == nil {
+        return 0
+    }
+    return max(getLongesthPath(node!.left) + 1, getLongesthPath(node!.right) + 1)
+}
+
+//: [Next](@next)
