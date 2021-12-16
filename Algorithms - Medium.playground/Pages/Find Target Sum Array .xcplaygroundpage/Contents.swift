fc09e2ccc2794bb508dc6372d369e35a8d4000c3
Chuy <chuynadamas@gmail.com>
2021-12-15 19:33:48 -0600

    Finding target

---
+++ Algorithms - Medium.playground/Pages/Find Target Sum Array .xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+//: [Previous](@previous)
+
+/**
+ 
+ You are given an integer array nums and an integer target.
+
+ You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+ For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+ Return the number of different expressions that you can build, which evaluates to target.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,1,1,1,1], target = 3
+ Output: 5
+ Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+ -1 + 1 + 1 + 1 + 1 = 3
+ +1 - 1 + 1 + 1 + 1 = 3
+ +1 + 1 - 1 + 1 + 1 = 3
+ +1 + 1 + 1 - 1 + 1 = 3
+ +1 + 1 + 1 + 1 - 1 = 3
+ Example 2:
+
+ Input: nums = [1], target = 1
+ Output: 1
+ 
+ 
+ */
+
+import Foundation
+
+func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
+    var prevSums: [Int] = [ nums[0], -nums[0] ]
+    var result = 0
+    for numIndex in 1..<nums.count {
+        var newPrevSums: [Int] = []
+        for prevSumIndex in 0..<prevSums.count {
+            newPrevSums.append(prevSums[prevSumIndex] + nums[numIndex])
+            newPrevSums.append(prevSums[prevSumIndex] - nums[numIndex])
+        }
+        prevSums = newPrevSums
+    }
+    
+    for config in prevSums {
+        if config == 3 {
+            result += 1
+        }
+    }
+    
+    return result
+}
+
+//: [Next](@next)
